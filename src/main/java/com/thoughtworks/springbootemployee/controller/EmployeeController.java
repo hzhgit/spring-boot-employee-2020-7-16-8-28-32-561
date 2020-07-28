e3f42727e1edb9f7e089ae6a0e3242c4a1cba261
zach-he <971499086@qq.com>
2020-07-28 23:31:37 +0800

    [zach]feature:add getEmployeesByPage mothod

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,30 +2,32 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.data.EmployeeData;
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    @GetMapping
-    public List<Employee> getEmployees() {
-        return new EmployeeData().getCompanies();
-    }
 
     @GetMapping(path = "/{id}")
     public Employee getEmployees(@PathVariable int id) {
         List<Employee> employees = new EmployeeData().getCompanies();
-        for (Employee employee : employees){
-            if(employee.getId() == id){
+        for (Employee employee : employees) {
+            if (employee.getId() == id) {
                 return employee;
             }
         }
         return null;
+    }
+
+    @GetMapping
+    public List<Employee> getEmployeesByPage(@RequestParam(name = "page", required = false) Integer page, @RequestParam(name = "pageSize", required = false) Integer pageSize) {
+        EmployeeData employeeData = new EmployeeData();
+        if (page != null && pageSize != null) {
+            return employeeData.getCompanies().subList(page - 1, pageSize - 1);
+        }
+        return employeeData.getCompanies();
     }
 
 
