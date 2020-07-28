13805cb02e77d299c1411b861ebcdf23fd8dd73e
zach-he <971499086@qq.com>
2020-07-28 21:01:14 +0800

    [zach]feature:add getCompanyById mothod

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +14,31 @@ import java.util.List;
 @RequestMapping("/companies")
 public class CompanyController {
     @GetMapping
-    public List<Company> getCompanies(){
+    public List<Company> getCompanies() {
         ArrayList<Company> companies = new ArrayList<>();
-        companies.add(new Company(1,"oocl"));
-        companies.add(new Company(2,"blibli"));
+        companies.add(new Company(1, "oocl"));
+        companies.add(new Company(2, "blibli"));
 
         ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
         ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
 
-        employeesOfOOCL.add(new Employee(1001,18,"zach","male",1000));
-        employeesOfOOCL.add(new Employee(1002,17,"alex","male",1000));
-        employeesOfBlibli.add(new Employee(1003,19,"zach1","male",1000));
-        employeesOfBlibli.add(new Employee(1004,18,"zach2","male",1000));
+        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000));
+        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000));
+        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000));
+        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000));
 
         companies.get(0).setEmployees(employeesOfOOCL);
         companies.get(1).setEmployees(employeesOfBlibli);
-
         return companies;
     }
+
+    @GetMapping(path = "/{id}")
+    public Company getCompanyById(@PathVariable int id) {
+        Company company = new Company(1, "OOCL");
+        if (company.getId() == id) {
+            return company;
+        }
+        return null;
+    }
+
 }
