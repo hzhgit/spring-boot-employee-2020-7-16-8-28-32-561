c2d87b7cd86443238f3fcc11589c07764c546f71
zach-he <971499086@qq.com>
2020-07-28 21:33:40 +0800

    [zach]feature:add getCompanyByPage mothod

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.data.CompanyData;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    @GetMapping
-    public List<Company> getCompanies() {
-        ArrayList<Company> companies = new ArrayList<>();
-        companies.add(new Company(1, "oocl"));
-        companies.add(new Company(2, "blibli"));
-
-        ArrayList<Employee> employeesOfOOCL = new ArrayList<>();
-        ArrayList<Employee> employeesOfBlibli = new ArrayList<>();
-
-        employeesOfOOCL.add(new Employee(1001, 18, "zach", "male", 1000));
-        employeesOfOOCL.add(new Employee(1002, 17, "alex", "male", 1000));
-        employeesOfBlibli.add(new Employee(1003, 19, "zach1", "male", 1000));
-        employeesOfBlibli.add(new Employee(1004, 18, "zach2", "male", 1000));
-
-        companies.get(0).setEmployees(employeesOfOOCL);
-        companies.get(1).setEmployees(employeesOfBlibli);
-        return companies;
-    }
-
     @GetMapping(path = "/{id}")
     public Company getCompanyById(@PathVariable int id) {
         Company company = new Company(1, "OOCL");
@@ -54,4 +36,12 @@ public class CompanyController {
         return null;
     }
 
+    @GetMapping
+    public List<Company> getCompanyByPage(@RequestParam(name = "page" , required = false) Integer page, @RequestParam(name = "pageSize",required = false) Integer pageSize) {
+        CompanyData companyData = new CompanyData();
+        if (page != null && pageSize != null) {
+            return companyData.getCompanies().subList(page-1, pageSize-1);
+        }
+        return companyData.getCompanies();
+    }
 }
---
+++ src/main/java/com/thoughtworks/springbootemployee/data/CompanyData.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.data;
+
+import com.thoughtworks.springbootemployee.model.Company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyData {
+    private List<Company> companies=new ArrayList<>();
+
+    public CompanyData() {
+        this.companies.add(new Company(1,"OOCL"));
+        this.companies.add(new Company(2,"BLIBLI"));
+        this.companies.add(new Company(3,"TW"));
+        this.companies.add(new Company(4,"AAA"));
+        this.companies.add(new Company(5,"BBB"));
+        this.companies.add(new Company(6,"CCC"));
+    }
+
+    public List<Company> getCompanies() {
+        return companies;
+    }
+}
